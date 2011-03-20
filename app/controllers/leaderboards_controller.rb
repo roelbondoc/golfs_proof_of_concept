d945a3a8951d8c24a3be247ced6e3b5f9ada61c5
Talha Syed <talha.syed@gmail.com>
2011-03-20 17:58:26 -0400

    Cup play;

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -3,14 +3,12 @@ class LeaderboardsController < ApplicationController
   def show
     @event = Golf::Event.find(params[:id])
     @matches = @event.matches
-    if @event.scoring_system && @event.scoring_system.downcase.include?("stroke")
-      # @partial = @event.status_id==4 ? 'final' : 'in_progress'
+    if @event.stroke_play?
       @qualified = @matches.qualified
       @not_qualified = @matches.not_qualified
       @withdrawn = @matches.withdrawn
-    # elsif(@event.scoring_system && @event.scoring_system.downcase.include?("cup"))
-    #   @partial = "cup_play"
-    #   @player_records = @event.player_records.all
+    elsif @event.cup_play?
+      render :cup_play
     # else
     #   @partial = "match_play"
     #   @player_records = @event.player_records.all(:include => [:rounds, :golfer1, :golfer2] )
