ddccffd24331b4adcead6aa6f4b4ff8c73063fb1
Roel Bondoc <rsbondoc@gmail.com>
2011-03-21 11:42:32 -0400

    Add golf match play partial

--- app/controllers/leaderboards_controller.rb
+++ app/controllers/leaderboards_controller.rb
@@ -9,9 +9,11 @@ class LeaderboardsController < ApplicationController
       @withdrawn = @matches.withdrawn
     elsif @event.cup_play?
       render :cup_play
-    # else
-    #   @partial = "match_play"
-    #   @player_records = @event.player_records.all(:include => [:rounds, :golfer1, :golfer2] )
+    elsif @event.match_play?
+      @matches = @matches.by_round(params[:round]) unless params[:round].blank?
+      @grouped_matches = @matches.group_by(&:round)
+      @round_range = params[:round].blank? ? @event.matches.max_round.downto(1) : Array(params[:round].to_i)
+      render :match_play
     end
 
     # @other_tournaments = Golf::Event.in_season(current_season).all(:conditions => {:scoring_system => "Stroke"}, :order => "start_date ASC", :select => ["id, league_id, scoring_system, tourn_name_short, prev_champion_id, start_date"])
